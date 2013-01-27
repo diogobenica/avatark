9b15e607672eea6fa6f3a7136772e66bd208a2f3
Diogo Benicá <diogobenica@gmail.com>
2013-01-27 19:00:11 -0200

    Add a default avatar when username doesn't exist in social networks

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -11,18 +11,32 @@ class Avatar
   def twitter
   	self.class.base_uri TWITTER_DOMAIN
   	response = get_image "/users/profile_image/#{@username}?size=original&redirect=false"
-    JSON.parse(response.parsed_response.to_json)["profile_image_url"]
+    unless response.index("body")
+      parsed_json = JSON.parse(response.parsed_response.to_json)
+      parsed_json["profile_image_url"] unless parsed_json["error"]
+    else
+      default_avatar
+    end
   end
 
   def facebook
     self.class.base_uri FACEBOOK_DOMAIN
     response = get_image "/#{@username}/picture?type=large&redirect=false"
-    JSON.parse(response.parsed_response.to_json)["data"]["url"]
+    parsed_json = JSON.parse(response.parsed_response.to_json)
+    unless parsed_json["error"]
+      parsed_json["data"]["url"]
+    else
+      default_avatar
+    end
   end
 
   private
 
   def get_image(resource)
   	self.class.get resource
+  end
+
+  def default_avatar
+    "http://wiseheartdesign.com/page_attachments/0000/0062/default-avatar.png"
   end
 end
