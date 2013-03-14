9b6066764937dc45682fb1f7c73dfa29f55dc854
Diogo Benicá <diogobenica@gmail.com>
2013-03-13 21:29:56 -0300

    Deploying with Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+require 'bundler/capistrano'
+
+set :application, "avatark"
+set :scm, :git
+set :repository,  "git://github.com/diogobenica/avatark.git"
+default_run_options[:pty] = true
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "198.211.110.248"                          # Your HTTP server, Apache/etc
+role :app, "198.211.110.248"                          # This may be the same as your `Web` server
+
+set :user, "root"
+set :deploy_to, "/var/www/avatark"
+
+set :use_sudo, false
+set :ssh_options, { :forward_agent => true }
+
+set :bundle_dir, ""
+set :bundle_flags, ""
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
