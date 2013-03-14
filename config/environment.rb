cfc8480a7fc5c7915100b972ec05a196c4088cdc
Diogo Benicá <diogo.pereira@abril.com.br>
2013-03-14 13:11:55 -0300

    Removing unnecessary  config

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.action_controller.relative_url_root
-
 # Initialize the rails application
 Avatark::Application.initialize!
