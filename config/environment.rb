50bbdc6c204b1b4ae4cf8ba4ec259e76e41df7f8
Diogo Benicá <diogobenica@gmail.com>
2013-03-14 00:22:35 -0300

    Last try to run Passenger of the night

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.action_controller.relative_url_root
+
 # Initialize the rails application
 Avatark::Application.initialize!
